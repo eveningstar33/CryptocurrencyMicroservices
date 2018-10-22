f9135853d6828187812b39681e6f45caa5b5f700
Dragos Sarpe <contact@usdgadget.com>
2018-10-22 18:55:08 +0300

    Added the config to pick up the values from application.properties

--- limits-service/src/main/java/com/dgs/microservices/limitsservice/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/dgs/microservices/limitsservice/LimitsConfigurationController.java
@@ -1,5 +1,6 @@
 package com.dgs.microservices.limitsservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +9,12 @@ import com.dgs.microservices.limitsservice.bean.LimitConfiguration;
 @RestController
 public class LimitsConfigurationController {
 
+	@Autowired
+	private Configuration configuration;
+	
 	@GetMapping("/limits")
 	public LimitConfiguration retrieveLimitsFromConfigurations() {
 		
-		return new LimitConfiguration(1000, 1);
+		return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
 	}
 }
