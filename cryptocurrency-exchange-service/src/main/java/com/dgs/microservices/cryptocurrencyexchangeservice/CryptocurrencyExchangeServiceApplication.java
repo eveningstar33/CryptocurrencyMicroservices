0877a313206528b4910725c73cb9511ed3ff31a0
Dragos Sarpe <contact@usdgadget.com>
2018-10-24 18:41:57 +0300

    Added the Cryptocurrency Exchange Microservice

---
+++ cryptocurrency-exchange-service/src/main/java/com/dgs/microservices/cryptocurrencyexchangeservice/CryptocurrencyExchangeServiceApplication.java
@@ -0,0 +1,12 @@
+package com.dgs.microservices.cryptocurrencyexchangeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptocurrencyExchangeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptocurrencyExchangeServiceApplication.class, args);
+	}
+}
