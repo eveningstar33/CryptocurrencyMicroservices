741ce982dbb4a0e9897d2ca203b7a3513834e6e3
Dragos Sarpe <contact@usdgadget.com>
2018-10-24 21:10:32 +0300

    Added Cryptocurrency Conversion Microservice

---
+++ cryptocurrency-conversion-service/src/main/java/com/dgs/microservices/cryptocurrencyconversionservice/CryptocurrencyConversionServiceApplication.java
@@ -0,0 +1,12 @@
+package com.dgs.microservices.cryptocurrencyconversionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptocurrencyConversionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptocurrencyConversionServiceApplication.class, args);
+	}
+}
