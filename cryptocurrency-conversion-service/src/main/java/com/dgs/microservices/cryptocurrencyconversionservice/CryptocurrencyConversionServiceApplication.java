58808ecab0a949cb7ba5e1cb2a2f2e709deb57b0
Dragos Sarpe <contact@usdgadget.com>
2018-10-26 18:17:05 +0300

    Connected Cryptocurrency Conversion Microservice to Eureka

--- cryptocurrency-conversion-service/src/main/java/com/dgs/microservices/cryptocurrencyconversionservice/CryptocurrencyConversionServiceApplication.java
+++ cryptocurrency-conversion-service/src/main/java/com/dgs/microservices/cryptocurrencyconversionservice/CryptocurrencyConversionServiceApplication.java
@@ -2,10 +2,12 @@ package com.dgs.microservices.cryptocurrencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.dgs.microservices.cryptocurrencyconversionservice")
+@EnableDiscoveryClient
 public class CryptocurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
