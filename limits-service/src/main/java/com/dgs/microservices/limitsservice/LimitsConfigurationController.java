3d7b4eb3448a2fb5b1a89a20986d6035bee9f075
Dragos Sarpe <contact@usdgadget.com>
2018-10-31 22:43:58 +0200

    Added fault tolerance with Hystrix

--- limits-service/src/main/java/com/dgs/microservices/limitsservice/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/dgs/microservices/limitsservice/LimitsConfigurationController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dgs.microservices.limitsservice.bean.LimitConfiguration;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class LimitsConfigurationController {
@@ -16,5 +17,23 @@ public class LimitsConfigurationController {
 	public LimitConfiguration retrieveLimitsFromConfigurations() {
 		
 		return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
+	}
+	
+	/*
+	To ilustrate Hystrix I create a new method
+	I try to replicate a scenario where one of these services throws an exception. So just because some other service that
+	depends on is not available or it's not able to connect to the database, or other reason it throws an exception, and if
+	this service is down, all the services that which are dependent on this are also go down and to prevent that we can
+	add fault tolerance.  
+	*/
+	
+	@GetMapping("/fault-tolerance-example")
+	@HystrixCommand(fallbackMethod="fallbackRetrieveConfiguration")
+	public LimitConfiguration retrieveConfiguration() {
+		throw new RuntimeException("Not available");
+	}
+	
+	public LimitConfiguration fallbackRetrieveConfiguration() {
+		return new LimitConfiguration(999, 9);
 	}
 }
